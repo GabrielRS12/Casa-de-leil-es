f85161a8f331a5091c0ed5edd44cc5d9266240f7
“Gabriel <gabrielronasimsen@gmail.com>
2024-05-23 16:54:15 -0300

    botão vender, filtro

--- src/View/Tela_inicial.java
+++ src/View/Tela_inicial.java
@@ -40,14 +40,12 @@ public class Tela_inicial extends javax.swing.JFrame {
         valor_txt = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         salvar_btn = new javax.swing.JButton();
-        jLabel5 = new javax.swing.JLabel();
-        status_txt = new javax.swing.JTextField();
         aviso1 = new javax.swing.JLabel();
         aviso2 = new javax.swing.JLabel();
-        aviso3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        lista_btn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel1.setText("Casa de Leilões ");
@@ -78,13 +76,6 @@ public class Tela_inicial extends javax.swing.JFrame {
             }
         });
 
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel5.setText("STATUS");
-        jLabel5.setToolTipText("Qual o item a ser cadastrado?");
-
-        status_txt.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        status_txt.setToolTipText("Como está produto?");
-
         aviso1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         aviso1.setForeground(new java.awt.Color(255, 51, 51));
         aviso1.setText("Nome invalido");
@@ -93,15 +84,11 @@ public class Tela_inicial extends javax.swing.JFrame {
         aviso2.setForeground(new java.awt.Color(255, 51, 51));
         aviso2.setText("Valor invalido, em números inteiros");
 
-        aviso3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        aviso3.setForeground(new java.awt.Color(255, 51, 51));
-        aviso3.setText("Status invalido, não digite números ");
-
-        jButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jButton1.setText("LISTA");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        lista_btn.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lista_btn.setText("LISTA");
+        lista_btn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                lista_btnActionPerformed(evt);
             }
         });
 
@@ -112,9 +99,7 @@ public class Tela_inicial extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(206, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(aviso3)
                     .addComponent(aviso2)
-                    .addComponent(valor_txt, javax.swing.GroupLayout.PREFERRED_SIZE, 280, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -129,9 +114,6 @@ public class Tela_inicial extends javax.swing.JFrame {
                                 .addComponent(aviso1))
                             .addGap(164, 164, 164))
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                            .addComponent(jLabel5)
-                            .addGap(281, 281, 281))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                             .addComponent(jLabel4)
                             .addGap(275, 275, 275))
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
@@ -141,8 +123,8 @@ public class Tela_inicial extends javax.swing.JFrame {
                         .addGroup(layout.createSequentialGroup()
                             .addComponent(salvar_btn)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton1))
-                        .addComponent(status_txt, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 280, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(lista_btn))
+                        .addComponent(valor_txt, javax.swing.GroupLayout.PREFERRED_SIZE, 280, javax.swing.GroupLayout.PREFERRED_SIZE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -163,38 +145,28 @@ public class Tela_inicial extends javax.swing.JFrame {
                 .addComponent(valor_txt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(aviso2)
-                .addGap(24, 24, 24)
-                .addComponent(jLabel5)
-                .addGap(18, 18, 18)
-                .addComponent(status_txt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(aviso3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 78, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(salvar_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lista_btn)
+                    .addComponent(salvar_btn))
+                .addGap(44, 44, 44))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     public void avisos(){
         aviso1.setVisible(false);
         aviso2.setVisible(false);
-        aviso3.setVisible(false);
+        
     }
     private void salvar_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salvar_btnActionPerformed
        Produto p = new Produto();
-       try{
        p.setValor(valor_txt.getText());
        p.setNome(item_txt.getText());
-       p.setStatus(status_txt.getText());
-       }catch (NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "Dados Invalidos", "Erro ao salvar" ,JOptionPane.ERROR_MESSAGE);
-            System.out.println(e);
-       }
-       if(p.getNome().matches("[a-zA-Z- \\p{L}]+") && p.getStatus().matches("[a-zA-Z- \\p{L}]+") && p.getValor().matches("[1-9][0-9]*" )){
+       p.setStatus("Não Vendido");
+       if(p.getNome().matches("[a-zA-Z- [1-9][0-9]\\p{L}]+") && p.getValor().matches("[1-9][0-9]*" )){
            try {
                if(ProdutoDAO.cadastrar(p)){
                    avisos();
@@ -205,17 +177,11 @@ public class Tela_inicial extends javax.swing.JFrame {
            }
        }
        else{
-           if(!p.getNome().matches("[a-zA-Z- \\p{L}]+")){
+           if(!p.getNome().matches("[a-zA-Z- [1-9][0-9]\\p{L}]+")){
            aviso1.setVisible(true);
            }
            else {
                aviso1.setVisible(false);
-           }
-           if(!p.getStatus().matches("[a-zA-Z- \\p{L}]+")){
-           aviso3.setVisible(true);
-           }
-           else{
-               aviso3.setVisible(false);
            }
            if(!p.getValor().matches("[1-9][0-9]*" )){
            aviso2.setVisible(true);
@@ -227,11 +193,11 @@ public class Tela_inicial extends javax.swing.JFrame {
         
     }//GEN-LAST:event_salvar_btnActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void lista_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_lista_btnActionPerformed
         Lista_Produtos lista_produtos = new Lista_Produtos();
         lista_produtos.setVisible(true);
         dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_lista_btnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -271,16 +237,13 @@ public class Tela_inicial extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel aviso1;
     private javax.swing.JLabel aviso2;
-    private javax.swing.JLabel aviso3;
     private javax.swing.JTextField item_txt;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
+    private javax.swing.JButton lista_btn;
     private javax.swing.JButton salvar_btn;
-    private javax.swing.JTextField status_txt;
     private javax.swing.JTextField valor_txt;
     // End of variables declaration//GEN-END:variables
 }
